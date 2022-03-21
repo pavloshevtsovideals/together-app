d4a1f100f34b7b4b1b0edd5b862c19c21f191872
pavloshevtsovideals <101900307+pavloshevtsovideals@users.noreply.github.com>
2022-03-21 13:23:15 +0200

    Update auth-provider-config.ts

--- config/custom/auth-provider-config.ts
+++ config/custom/auth-provider-config.ts
@@ -5,7 +5,7 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 
 export const authProviderConfig: Nullable<AuthProviderConfig> = {
-  provider: GooleProvider({
+  provider: GoogleProvider({
     clientId: 413446987194-a0vn73aptj04j0sua62310chf3vkbhtr.apps.googleusercontent.com,
     clientSecret: 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,
   }),
